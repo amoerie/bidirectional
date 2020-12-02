a33eb6132fb2fedaab52d1012e3f5ee830134cc7
Pieter-Jan Van Robays <pieterjan.vanrobays@gmail.com>
2020-12-02 19:28:25 +0100

    first test with 1MB file success

--- Bidirectional.Demo.Common/Contracts/Client/GetFile/GetFileRequest.cs
+++ Bidirectional.Demo.Common/Contracts/Client/GetFile/GetFileRequest.cs
@@ -1,13 +1,18 @@
-﻿using System;
+﻿using ProtoBuf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Bidirectional.Demo.Common.Contracts.Client.GetFile
 {
+    [ProtoContract]
     public class GetFileRequest
     {
+        [ProtoMember(1)]
         public string Path { get; set; }
+
+        [ProtoMember(2)]
         public int? RequestedSizeInBytes { get; set; }
     }
 }
--- Bidirectional.Demo.Common/Contracts/Client/GetFile/GetFileResponse.cs
+++ Bidirectional.Demo.Common/Contracts/Client/GetFile/GetFileResponse.cs
@@ -1,14 +1,21 @@
-﻿using System;
+﻿using ProtoBuf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Bidirectional.Demo.Common.Contracts.Client.GetFile
 {
+    [ProtoContract]
     public class GetFileResponse
     {
+        [ProtoMember(1)]
         public byte[] Bytes { get; set; }
+
+        [ProtoMember(2)]
         public string FileName { get; set; }
+
+        [ProtoMember(3)]
         public long? GenerationTimeInMs { get; set; }
     }
 }
