c80e112196c29ba158a1cda6f169d48499c4d1e2
Pieter-Jan Van Robays <pieterjan.vanrobays@gmail.com>
2024-10-24 14:35:52 +0200

    simple filesystem models

---
+++ Bidirectional.Perf.SignalR.Client/Utils/FileManager.cs
@@ -0,0 +1,16 @@
+﻿using Bidirectional.Perf.SignalR.Contracts;
+
+namespace Bidirectional.Perf.SignalR.Client.Utils;
+
+public sealed class FileManager
+{
+    Task<DirectoryDto> GetPathAsync(string path, int maxDepth = 3)
+    {
+        return GetDirectoryInfoRecursive(path, maxDepth);
+    }
+
+    private Task<DirectoryDto> GetDirectoryInfoRecursive(string path, int maxDepth)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Bidirectional.Perf.SignalR.Contracts/FileSystem.cs
@@ -0,0 +1,19 @@
+﻿namespace Bidirectional.Perf.SignalR.Contracts;
+
+public record FileSystemBase
+{
+    public string Name { get; set; } = default!;
+    public DateTime CreationDateTime { get; set; }
+    public DateTime LastUpdateDateTime { get; set; }
+}
+
+public sealed record FileDto : FileSystemBase
+{
+    public long Size { get; set; }
+}
+
+public sealed record DirectoryDto : FileSystemBase
+{
+    public IList<FileSystemBase> Files { get; set; } = new List<FileSystemBase>();
+    public IList<FileSystemBase> Directories { get; set; } = new List<FileSystemBase>();
+}
