46366717b8a497743fa56e2b30346525a0067350
Pieter-Jan Van Robays <pieterjan.vanrobays@gmail.com>
2024-10-24 14:44:06 +0200

    implement a simple FileManager

--- Bidirectional.Perf.SignalR.Client/Utils/FileManager.cs
+++ Bidirectional.Perf.SignalR.Client/Utils/FileManager.cs
@@ -4,13 +4,41 @@ namespace Bidirectional.Perf.SignalR.Client.Utils;
 
 public sealed class FileManager
 {
-    Task<DirectoryDto> GetPathAsync(string path, int maxDepth = 3)
+    public static DirectoryDto GetPath(string path, int maxDepth = 3)
     {
         return GetDirectoryInfoRecursive(path, maxDepth);
     }
 
-    private Task<DirectoryDto> GetDirectoryInfoRecursive(string path, int maxDepth)
+    private static DirectoryDto GetDirectoryInfoRecursive(string path, int maxDepth)
     {
-        throw new NotImplementedException();
+        var directory = new DirectoryInfo(path);
+        if (!directory.Exists) throw new InvalidOperationException("Directory does not exist: " + path);
+
+        var directoryDto = new DirectoryDto
+        {
+            Name = directory.Name,
+            CreationDateTime = directory.CreationTime,
+            LastUpdateDateTime = directory.LastWriteTime
+        };
+
+        if (maxDepth <= 0) return directoryDto;
+        
+        foreach (var subDirectory in directory.GetDirectories())
+        {
+            directoryDto.Directories.Add(GetDirectoryInfoRecursive(subDirectory.FullName, maxDepth - 1));
+        }
+
+        foreach (var file in directory.GetFiles())
+        {
+            directoryDto.Files.Add(new FileDto
+            {
+                Name = file.Name,
+                CreationDateTime = file.CreationTime,
+                LastUpdateDateTime = file.LastWriteTime,
+                Size = file.Length
+            });
+        }
+
+        return directoryDto;
     }
 }
